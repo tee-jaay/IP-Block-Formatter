e79e78284906fcd12993d64dfa1d22286b0e5d65
Tam Jid <tamjid.mdh@gmail.com>
2023-12-07 14:23:28 +0600

    save to csv file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import csv
+
 # Open the text file containing the IP addresses
 with open('ip_addresses.txt', 'r') as txt_file:
     # Read the contents of the file
@@ -12,8 +14,12 @@ ip_addresses = [line.split()[0] for line in lines if line.strip()]
 # Convert the list of IP addresses to a set to remove duplicates
 unique_ip_addresses = set(ip_addresses)
 
-# Join the unique IP addresses back together with each address on a separate line, prefixed with "deny from"
-formatted_data = '\n'.join('deny from ' + ip for ip in unique_ip_addresses)
+# Format the IP addresses with "deny from" prefix
+formatted_data = [['deny from ' + ip] for ip in unique_ip_addresses]
 
-# Print the formatted data
-print(formatted_data)
+# Write the formatted data to a CSV file
+with open('formatted_ip_addresses.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerows(formatted_data)
+
+print("Formatted IP addresses written to 'formatted_ip_addresses.csv'")
